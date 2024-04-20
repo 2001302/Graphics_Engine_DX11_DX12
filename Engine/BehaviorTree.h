95d1fd27d890a4ed345ef13463ed18d4836848ad
kyo <yoonjunkyo@gmail.com>
2024-04-20 15:56:04 +0900

    refactorig : add behavior tree root node invoker

--- Engine/BehaviorTree.h
+++ Engine/BehaviorTree.h
@@ -2,124 +2,137 @@
 
 #include "header.h"
 
-enum EnumBehaviorTreeStatus
+namespace BehaviorTree
 {
-	eSuccess = 0,
-	eFail = 1,
-};
-
-class BehaviorTreeBuilder
-{
-private:
-	std::vector<IBehaviorTreeNode*> Nodes;
-
-public:
-	SequenceNode* Sequence()
+	enum EnumBehaviorTreeStatus
 	{
-		auto sequence = new SequenceNode();
-		sequence->SetBuilder(this);
-		Nodes.push_back(dynamic_cast<IBehaviorTreeNode*>(sequence));
-		return sequence;
-	}
-	SelectorNode* Selector()
-	{
-		auto selector = new SelectorNode();
-		selector->SetBuilder(this);
-		Nodes.push_back(dynamic_cast<IBehaviorTreeNode*>(selector));
-		return selector;
-	}
-};
-
-class IBehaviorTreeNode 
-{
-public:
-	virtual EnumBehaviorTreeStatus Invoke()
-	{
-		return EnumBehaviorTreeStatus::eSuccess;
-	}
-
-	void SetParent(IBehaviorTreeNode* node)
-	{
-		parentNode = node;
-	}
-
-private:
-	IBehaviorTreeNode* parentNode;
-};
-
-class BehaviorTreeRootNode : public IBehaviorTreeNode
-{
-public:
-	BehaviorTreeRootNode* Excute(IBehaviorTreeNode* node)
-	{
-		node->SetParent(this);
-		childNodes.push_back(node);
-		return this;
-	}
-	BehaviorTreeBuilder* Close() 
-	{
-		return builder;
-	}
-	void SetBuilder(BehaviorTreeBuilder* other)
-	{
-		builder = other;
-	}
-
-protected:
-	std::vector<IBehaviorTreeNode*> childNodes;
-	BehaviorTreeBuilder* builder;
-};
-
-class SequenceNode : public BehaviorTreeRootNode
-{
-	EnumBehaviorTreeStatus Invoke() override
-	{
-		for (auto& child : childNodes)
-		{
-			if (child->Invoke() == EnumBehaviorTreeStatus::eFail)
-				return EnumBehaviorTreeStatus::eFail;
-		}
-		return EnumBehaviorTreeStatus::eSuccess;
-	}
-};
-
-class SelectorNode : public BehaviorTreeRootNode
-{
-public:
-	EnumBehaviorTreeStatus Invoke() override
-	{
-		for (auto& child : childNodes)
-		{
-			if (child->Invoke() == EnumBehaviorTreeStatus::eSuccess)
-				return EnumBehaviorTreeStatus::eSuccess;
-		}
-		return EnumBehaviorTreeStatus::eSuccess;
-	}
-};
-
-class ActionNode : public IBehaviorTreeNode
-{
-
-};
-
-class Test
-{
-public: 
-	void TestFunc()
-	{
-		auto builder = new BehaviorTreeBuilder();
-
-		builder
-			->Sequence()
-				->Excute(new ActionNode())
-				->Excute(new ActionNode())
-				->Excute(new ActionNode())
-			->Close()
-			->Selector()
-				->Excute(new ActionNode())
-				->Excute(new ActionNode())
-				->Excute(new ActionNode())
-			->Close();
-			
+		eSuccess = 0,
+		eFail = 1,
 	};
-};
+
+	class IBehaviorTreeNode
+	{
+	public:
+		virtual EnumBehaviorTreeStatus Invoke()
+		{
+			return EnumBehaviorTreeStatus::eSuccess;
+		}
+
+		void SetParent(IBehaviorTreeNode* node)
+		{
+			parentNode = node;
+		}
+
+	protected:
+		IBehaviorTreeNode* parentNode;
+	};
+
+	class BehaviorTreeRootNode : public IBehaviorTreeNode
+	{
+	public:
+		BehaviorTreeRootNode* Excute(IBehaviorTreeNode* node)
+		{
+			node->SetParent(this);
+			childNodes.push_back(node);
+			return this;
+		}
+
+		SequenceNode* Sequence()
+		{
+			SequenceNode* node = new SequenceNode();
+			childNodes.push_back(dynamic_cast<IBehaviorTreeNode*>(node));
+			return node;
+		}
+
+		SelectorNode* Selector()
+		{
+			SelectorNode* node = new SelectorNode();
+			childNodes.push_back(dynamic_cast<IBehaviorTreeNode*>(node));
+			return node;
+		}
+
+		BehaviorTreeRootNode* Close()
+		{
+			return dynamic_cast<BehaviorTreeRootNode*>(parentNode);
+		}
+
+	protected:
+		std::vector<IBehaviorTreeNode*> childNodes;
+	};
+
+	class SequenceNode : public BehaviorTreeRootNode
+	{
+		EnumBehaviorTreeStatus Invoke() override
+		{
+			for (auto& child : childNodes)
+			{
+				if (child->Invoke() == EnumBehaviorTreeStatus::eFail)
+					return EnumBehaviorTreeStatus::eFail;
+			}
+			return EnumBehaviorTreeStatus::eSuccess;
+		}
+	};
+
+	class SelectorNode : public BehaviorTreeRootNode
+	{
+	public:
+		EnumBehaviorTreeStatus Invoke() override
+		{
+			for (auto& child : childNodes)
+			{
+				if (child->Invoke() == EnumBehaviorTreeStatus::eSuccess)
+					return EnumBehaviorTreeStatus::eSuccess;
+			}
+			return EnumBehaviorTreeStatus::eSuccess;
+		}
+	};
+
+	class ActionNode : public IBehaviorTreeNode
+	{
+
+	};
+
+	class BehaviorTreeBuilder
+	{
+	public:
+		BehaviorTreeRootNode* Build()
+		{
+			m_Tree = new BehaviorTreeRootNode();
+			return m_Tree;
+		}
+		void Run() 
+		{
+			m_Tree->Invoke();
+		}
+	private:
+		BehaviorTreeRootNode* m_Tree;
+	};
+
+
+	class Test
+	{
+	public:
+		void TestFunc()
+		{
+			auto builder = new BehaviorTreeBuilder();
+
+			builder->Build()
+				->Sequence()
+					->Excute(new ActionNode())
+					->Sequence()
+						->Excute(new ActionNode())
+						->Excute(new ActionNode())
+					->Close()
+					->Excute(new ActionNode())
+					->Excute(new ActionNode())
+				->Close()
+				->Selector()
+					->Excute(new ActionNode())
+					->Excute(new ActionNode())
+					->Excute(new ActionNode())
+				->Close();
+
+			builder->Run();
+		};
+	};
+}

56360b758483273d305937e523cfc2a7a01afd41
kyo <yoonjunkyo@gmail.com>
2024-06-15 11:14:36 +0900

    update gui

--- Engine/panel.h
+++ Engine/panel.h
@@ -5,10 +5,25 @@
 
 namespace ed = ax::NodeEditor;
 
+#ifdef _MSC_VER
+#define portable_strcpy strcpy_s
+#define portable_sprintf sprintf_s
+#else
+#define portable_strcpy strcpy
+#define portable_sprintf sprintf
+#endif
+
 namespace Engine {
 class Panel : public BaseGui {
+
+    struct LinkInfo {
+        ed::LinkId Id;
+        ed::PinId InputId;
+        ed::PinId OutputId;
+    };
+
   public:
-    //void OnStart() override;
+    void OnStart() override;
     void OnFrame(float deltaTime) override;
 
     float m_shininess = 1.0f;
@@ -32,6 +47,14 @@ class Panel : public BaseGui {
     Light m_lightFromGUI;
     float m_materialDiffuse = 1.0f;
     float m_materialSpecular = 1.0f;
+
+    
+    ed::EditorContext *m_Context = nullptr;
+    ImVector<LinkInfo> m_Links; // List of live links. It is dynamic unless you
+                                // want to create read-only view over nodes.
+    int m_NextLinkId =
+        100; // Counter to help generate link ids. In real application this will
+             // probably based on pointer to user data structure.
 };
 } // namespace Engine
 #endif
